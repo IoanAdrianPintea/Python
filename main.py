546a2c5db2985e31a6344559a4df02b0bf294175
IoanAdrianPintea <116074866+IoanAdrianPintea@users.noreply.github.com>
2023-01-22 18:25:29 +0200

    exercitiu platforma

---
+++ main.py
@@ -0,0 +1,10 @@
+def ex_0332():
+    n = int(input())
+    suma = 0
+    for i in range(1, n + 1):
+        suma += i * (i + 1)
+    print("Rezultatul este", suma)
+
+
+if __name__ == '__main__':
+    ex_0332()
